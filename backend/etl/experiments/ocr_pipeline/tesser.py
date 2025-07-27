231977cdef690f527720b9cb48ffd50ecd03d348
karandaryanani <kdaryanani@hks.harvard.edu>
2025-07-27 15:04:11 -0400

    testing home directory tesseract installation

---
+++ backend/etl/experiments/ocr_pipeline/tesser.py
@@ -0,0 +1,17 @@
+import pytesseract
+import shutil
+import subprocess
+
+# Explicitly set tesseract binary location (adjust the path if your username is different)
+pytesseract.pytesseract.tesseract_cmd = '/n/home04/kdaryanani/local/bin/tesseract'
+
+# Print the path pytesseract will use
+print("Tesseract binary:", pytesseract.pytesseract.tesseract_cmd)
+print("which tesseract:", shutil.which("tesseract"))
+
+# Print Tesseract version as seen by pytesseract
+try:
+    version_output = subprocess.check_output([pytesseract.pytesseract.tesseract_cmd, '--version'], text=True)
+    print("Tesseract version output:\n", version_output)
+except Exception as e:
+    print("Failed to run tesseract:", e)
